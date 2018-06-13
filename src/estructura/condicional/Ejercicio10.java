1d46714eadf2b36e38307d90019225cf3c5e5510
mlennard-utn <39415012+mlennard-utn@users.noreply.github.com>
2018-06-13 20:51:07 -0300

    Update Ejercicio10.java
    Modificacion archivo 10

--- src/estructura/condicional/Ejercicio10.java
+++ src/estructura/condicional/Ejercicio10.java
@@ -2,13 +2,14 @@ package estructura.condicional;
 
 /**
  * 10. Programa que lea una variable entera mes y compruebe si el valor 
- * corresponde a un mes de 30, 31 o 28 dÌas. Se mostrar· adem·s el nombre del mes. 
- * Se debe comprobar que el valor introducido estÈ comprendido entre 1 y 12.
+ * corresponde a un mes de 30, 31 o 28 d√≠as. Se mostrar√° adem√°s el nombre del mes. 
+ * Se debe comprobar que el valor introducido est√© comprendido entre 1 y 12.
  */
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
+		// Se hace la modificacion del archivo
 
 	}
 
